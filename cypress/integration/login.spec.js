b54cb4178a9c4da1dc333c0640b174aa0798310e
Anja2323 <anjamattucker@gmail.com>
2022-04-28 23:04:47 +0200

    Login and register added

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -3,19 +3,80 @@
 
 
 describe('login test', () => {
-    xit('visit gallery app', () => {
+    it('visit gallery app', () => {
         cy.visit('https://gallery-app.vivifyideas.com/');
         cy.url().should('eq', 'https://gallery-app.vivifyideas.com/');
+        cy.wait(500);
     })
-    xit('click on login button', () =>{
+    it('click on login button', () =>{
         cy.get('a[href="/login"]').click();
+        cy.wait(500);
     })
-    it.only('login with valid credentials', () => {
+    it('login with no email ', () => {
+        cy.visit('/login');
+        cy.url().should('include', '/login');
+        cy.get('#password').type('kakosimiti23');
+        cy.get('button[type="submit"]').click();
+        cy.url().should('include', '/login');
+        cy.wait(1000);
+    })
+    it('login with invalid email (no @ )', () => {
+        cy.visit('/login');
+        cy.url().should('include', '/login');
+        cy.get('#email').type('jelenajelena861yahoo.com');
+        cy.get('#password').type('kakosimiti23');
+        cy.get('button[type="submit"]').click();
+        cy.wait(1000);
+    })
+    it('login with invalid email (no .com )', () => {
+        cy.visit('/login');
+        cy.url().should('include', '/login');
+        cy.get('#email').type('jelenajelena86@yahoo');
+        cy.get('#password').type('kakosimiti23');
+        cy.get('button[type="submit"]').click();
+        cy.wait(1000);
+    })
+    it('login with no password', () => {
+        cy.visit('/login');
+        cy.url().should('include', '/login');
+        cy.get('#email').type('jelenajelena861@yahoo.com');
+        cy.get('button[type="submit"]').click();
+        cy.wait(1000);
+    })
+    
+    it('login with invalid password (no numbers)', () => {
+        cy.visit('/login');
+        cy.url().should('include', '/login');
+        cy.get('#email').type('jelenajelena861@yahoo.com');
+        cy.get('#password').type('kakosimiti');
+        cy.get('button[type="submit"]').click();
+        cy.wait(1000);
+    })
+    
+
+    it('login with invalid password (only 3 chars )', () => {
+        cy.visit('/login');
+        cy.url().should('include', '/login');
+        cy.get('#email').type('jelenajelena861@yahoo.com');
+        cy.get('#password').type('kak');
+        cy.get('button[type="submit"]').click();
+        cy.wait(1000);
+    })
+
+    it('login with no password and email', () => {
+        cy.visit('/login');
+        cy.url().should('include', '/login');
+        cy.get('button[type="submit"]').click();
+        cy.wait(1000);
+    })
+       
+    it('login with valid credentials', () => {
         cy.visit('/login');
         cy.url().should('include', '/login');
         cy.get('#email').type('jelenajelena861@yahoo.com');
         cy.get('#password').type('kakosimiti23');
         cy.get('button[type="submit"]').click();
+        cy.wait(1000);
     })
     it('logout', () =>{
         //cy.wait(500);
--- cypress/integration/register.spec.js
+++ cypress/integration/register.spec.js
@@ -26,57 +26,105 @@ describe('test register', () => {
         })
 
     it('register with invalid email(without @)', () => {
-            cy.visit('/register');
-            cy.url().should('include', '/register');
-            cy.get('#first-name').type('Anja');
-            cy.get('#last-name').type('Krstic');
-            cy.get('#email').type('axnjfamattuckergmail.com');
-            cy.get('#password').type('jasamanja23');
-            cy.get('#password-confirmation').type('jasamanja23');
-            cy.get('[type="checkbox"]').check();
-            cy.get('button[type="submit"]').click();
-            cy.url().should('include', '/register');
-            })
+         cy.visit('/register');
+         cy.url().should('include', '/register');
+         cy.get('#first-name').type('Anja');
+         cy.get('#last-name').type('Krstic');
+         cy.get('#email').type('axnjfamattuckergmail.com');
+         cy.get('#password').type('jasamanja23');
+         cy.get('#password-confirmation').type('jasamanja23');
+         cy.get('[type="checkbox"]').check();
+         cy.get('button[type="submit"]').click();
+         cy.url().should('include', '/register');
+         })
+
+    it('register with invalid email(without .com)', () => {
+         cy.visit('/register');
+         cy.url().should('include', '/register');
+         cy.get('#first-name').type('Anja');
+         cy.get('#last-name').type('Krstic');
+         cy.get('#email').type('axnjfamattucker@gmail');
+         cy.get('#password').type('jasamanja23');
+         cy.get('#password-confirmation').type('jasamanja23');
+         cy.get('[type="checkbox"]').check();
+         cy.get('button[type="submit"]').click();
+         cy.url().should('include', '/register');
+         })
 
     it('register with invalid password(no number)', () =>{
-            cy.visit('/register');
-            cy.url().should('include', '/register');
-            cy.get('#first-name').type('Anja');
-            cy.get('#last-name').type('Krstic');
-            cy.get('#email').type('axnjfamattuckergmail.com');
-            cy.get('#password').type('jasamanja');
-            cy.get('#password-confirmation').type('jasamanja23');
-            cy.get('[type="checkbox"]').check();
-            cy.get('button[type="submit"]').click();
-            cy.url().should('include', '/register');
-            })
+         cy.visit('/register');
+         cy.url().should('include', '/register');
+         cy.get('#first-name').type('Anja');
+         cy.get('#last-name').type('Krstic');
+         cy.get('#email').type('axnjfamattuckergmail.com');
+         cy.get('#password').type('jasamanja');
+         cy.get('#password-confirmation').type('jasamanja23');
+         cy.get('[type="checkbox"]').check();
+         cy.get('button[type="submit"]').click();
+         cy.url().should('include', '/register');
+         })
            
-     it('register with invalid password(no password)', () =>{
-                cy.visit('/register');
-                cy.url().should('include', '/register');
-                cy.get('#first-name').type('Anja');
-                cy.get('#last-name').type('Krstic');
-                cy.get('#email').type('axnjfamattuckergmail.com');
-                cy.get('#password-confirmation').type('jasamanja23');
-                cy.get('[type="checkbox"]').check();
-                cy.get('button[type="submit"]').click();
-                cy.url().should('include', '/register');
-                })
+    it('register with invalid password(no password)', () =>{
+         cy.visit('/register');
+         cy.url().should('include', '/register');
+         cy.get('#first-name').type('Anja');
+         cy.get('#last-name').type('Krstic');
+         cy.get('#email').type('axnjfamattuckergmail.com');
+         cy.get('#password-confirmation').type('jasamanja23');
+         cy.get('[type="checkbox"]').check();
+         cy.get('button[type="submit"]').click();
+         cy.url().should('include', '/register');
+         })
 
-                it('register with invalid password(1 char)', () =>{
-                    cy.visit('/register');
-                    cy.url().should('include', '/register');
-                    cy.get('#first-name').type('Anja');
-                    cy.get('#last-name').type('Krstic');
-                    cy.get('#email').type('axnjfamattuckergmail.com');
-                    cy.get('#password').type('j');
-                    cy.get('#password-confirmation').type('jasamanja23');
-                    cy.get('[type="checkbox"]').check();
-                    cy.get('button[type="submit"]').click();
-                    cy.url().should('include', '/register');
-                    })
-        
+    it('register with invalid password(1 char)', () =>{
+         cy.visit('/register');
+         cy.url().should('include', '/register');
+         cy.get('#first-name').type('Anja');
+         cy.get('#last-name').type('Krstic');
+         cy.get('#email').type('axnjfamattuckergmail.com');
+         cy.get('#password').type('j');
+         cy.get('#password-confirmation').type('jasamanja23');
+         cy.get('[type="checkbox"]').check();
+         cy.get('button[type="submit"]').click();
+         cy.url().should('include', '/register');
+         })
+                   
+    it('register with no password confirmation', () => {
+         cy.visit('/register');
+         cy.url().should('include', '/register');
+         cy.get('#first-name').type('Anja');
+         cy.get('#last-name').type('Krstic');
+         cy.get('#email').type('axnjfamattucker@gmail.com');
+         cy.get('#password').type('jasamanja23');
+         cy.get('[type="checkbox"]').check();
+         cy.get('button[type="submit"]').click();
+         cy.url().should('include', '/register');
+         })
    
+    it('register with not valid password confirmation', () => {
+         cy.visit('/register');
+         cy.url().should('include', '/register');
+         cy.get('#first-name').type('Anja');
+         cy.get('#last-name').type('Krstic');
+         cy.get('#email').type('axnjfamattucker@gmail.com');
+         cy.get('#password').type('jasamanja23');
+         cy.get('#password-confirmation').type('jasamanja');
+         cy.get('[type="checkbox"]').check();
+         cy.get('button[type="submit"]').click();
+         cy.url().should('include', '/register');
+         })
+                            
+    it('not accepted terms and conditions', () => {
+         cy.visit('/register');
+         cy.url().should('include', '/register');
+         cy.get('#first-name').type('Anja');
+         cy.get('#last-name').type('Krstic');
+         cy.get('#email').type('axnjfamattucker@gmail.com');
+         cy.get('#password').type('jasamanja23');
+         cy.get('#password-confirmation').type('jasamanja23');
+         cy.get('button[type="submit"]').click();
+         cy.url().should('include', '/register');
+         })
 
     it('register with valid data', () => {
         cy.visit('/register');
